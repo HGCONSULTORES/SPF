cbbf88b8e2c88c69dc774f3f4d814746b30e7350
HGCONSULTORES <39781146+HGCONSULTORES@users.noreply.github.com>
2021-08-04 18:35:02 -0500

    Nuevo Modulo
    Nuevo Modulo

---
+++ job_order_material_consumption/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of GECOERP. See LICENSE file for full copyright and licensing details.
+
+from . import stock_picking
+from . import project_task
+from . import consumption_type
